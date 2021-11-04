443104cf86beefa3509fa9c92a604574e41b1ff8
Aivaras Gluodas <aivaras.gluodas@teliacompany.com>
2021-11-04 13:09:19 +0200

    #45: Force proxy

--- src/services/slack-client/slack-client.ts
+++ src/services/slack-client/slack-client.ts
@@ -1,10 +1,17 @@
 import { WebClient } from '@slack/web-api';
+import createHttpsProxyAgent from 'https-proxy-agent';
 import { InitializeClientOptions, LookUpUserByEmailOptions } from './types';
 
 let slackClient: WebClient;
 
 export const initializeClient = (options: InitializeClientOptions) => {
-  slackClient = new WebClient(options.token);
+  const { https_proxy } = process.env;
+
+  const agent = https_proxy ? createHttpsProxyAgent(https_proxy) : undefined;
+
+  slackClient = new WebClient(options.token, {
+    agent,
+  });
 };
 
 export const lookUpUserByEmail = (options: LookUpUserByEmailOptions) => {
